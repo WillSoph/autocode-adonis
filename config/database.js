2706cdf67b7493fcfeedd3cac602b077e250d125
Willians Ferreira <wdesenvolvimento@outlook.com>
2020-07-12 10:18:07 -0300

    config adonis

--- config/database.js
+++ config/database.js
@@ -1,25 +1,17 @@
 'use strict'
-// const Url = require('url-parse')
+const Url = require('url-parse')
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
-// const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
 module.exports = {
-  /*
-  |--------------------------------------------------------------------------
-  | Default Connection
-  |--------------------------------------------------------------------------
-  |
-  | Connection defines the default connection settings to be used while
-  | interacting with SQL databases.
-  |
-  */
+
   connection: Env.get('DB_CONNECTION', 'sqlite'),
 
   /*
@@ -55,16 +47,16 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-      // host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      // host: Env.get('DB_HOST', 'localhost'),
       // port: Env.get('DB_PORT', ''),
-      // user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      // password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      // database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+      // user: Env.get('DB_USER', 'root'),
+      // password: Env.get('DB_PASSWORD', ''),
+      // database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
       // host: "us-cdbr-east-05.cleardb.net",
       // user: "b5412e08d8468d",
       // password: "9479261d",
