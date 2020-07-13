e1c8bf0cfa6b6db3209b216384998cfc03c1dc6c
Willians Ferreira <wdesenvolvimento@outlook.com>
2020-07-13 13:59:46 -0300

    correcao

--- app/Controllers/Http/RegisterController.js
+++ app/Controllers/Http/RegisterController.js
@@ -17,7 +17,7 @@ class RegisterController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {
   }
 
   /**
@@ -29,7 +29,7 @@ class RegisterController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
+  async create({ request, response, view }) {
   }
 
   /**
@@ -40,7 +40,12 @@ class RegisterController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request }) {
+    const data = request.only(['name', 'email', 'password'])
+
+    const user = await User.create(data)
+
+    return user
   }
 
   /**
@@ -52,7 +57,7 @@ class RegisterController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
   }
 
   /**
@@ -64,7 +69,7 @@ class RegisterController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
+  async edit({ params, request, response, view }) {
   }
 
   /**
@@ -75,7 +80,7 @@ class RegisterController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
   }
 
   /**
@@ -86,7 +91,7 @@ class RegisterController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
   }
 }
 
