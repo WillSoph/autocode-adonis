ad0d04b83d159ce3c9ba1b4f6c0f56977be8a227
Willians Ferreira <wdesenvolvimento@outlook.com>
2020-07-13 14:30:15 -0300

    correcao

--- app/Controllers/Http/RegisterController.js
+++ app/Controllers/Http/RegisterController.js
@@ -40,7 +40,7 @@ class RegisterController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request }) {
+  async store({ request, response }) {
     const data = request.only(['name', 'email', 'password'])
 
     const user = await User.create(data)
